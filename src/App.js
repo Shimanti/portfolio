b871677ba03e7ca3b7e1cf8445bfd706ebc2b668
Shimanti <676501+Shimanti@users.noreply.github.com>
2022-04-13 22:04:22 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react"
 import Hero from "./Hero"
-import Projects from "./Projects"
-import Contact from "./Contact"
   
 export default function App() {
 
@@ -12,8 +10,6 @@ export default function App() {
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </header>
       <Hero />
-      {/* <Projects />
-      <Contact /> */}
 
     </main>
   )
